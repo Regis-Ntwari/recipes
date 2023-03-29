de9db064bcf69e1a15d22f05ebb3124dee0bef7a
Regis-Ntwari <rex90danny@gmail.com>
2023-03-29 20:19:15 +0200

    change recipe filename causing error

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/category.dart';
-import '../models/recipe.dart';
+import '../models/Recipe.dart';
 
 const DUMMY_CATEGORIES = [
   Category(
